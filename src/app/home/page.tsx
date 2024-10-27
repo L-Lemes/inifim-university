71b220e9ea2f498fcd0d164957e5108ae167a258
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-27 18:11:46 -0300

    fix(homePage): correct const isTabOpen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ import { ConfigurationTab } from "../_components/ConfigurationsTab";
 import { UserTab } from "../_components/UserTab";
 import { NavTab } from "../_components/NavTab";
 
-import { TabsControlProvider, useTabsControl } from "@/hooks/useTabsControl";
+import { useTabsControl } from "@/hooks/useTabsControl";
 
 import { CarouselSection } from "../_components/CarouselSection";
 import { SearchBar } from "../_components/SearchBar";
@@ -13,14 +13,13 @@ import { SearchBar } from "../_components/SearchBar";
 export default function House() {
   const { showTabOpen } = useTabsControl();
 
-  const isModalOpen = Object.values(showTabOpen).some(
-    (value) => value === true
-  );
+  const isTabOpen =
+    showTabOpen && Object.values(showTabOpen).some((value) => value === true);
 
   return (
     <div
       className={`relative  bg-[rgba(0,0,0,0.75)] p-6 overflow-hidden ${
-        isModalOpen ? "h-[100dvh]" : "h-full"
+        isTabOpen ? "h-[100dvh]" : "h-full"
       }`}
     >
       <Header />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
+import { TabsControlProvider } from "@/hooks/useTabsControl";
 
-import { Header } from "./_components/Header";
-import { ConfigurationTab } from "./_components/ConfigurationsTab";
-import { UserTab } from "./_components/UserTab";
-import { NavTab } from "./_components/NavTab";
-
-import { TabsControlProvider, useTabsControl } from "@/hooks/useTabsControl";
-
-import { CarouselSection } from "./_components/CarouselSection";
-import { SearchBar } from "./_components/SearchBar";
 import House from "./home/page";
 
 export default function Home() {
