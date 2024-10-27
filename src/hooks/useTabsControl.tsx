e96e424b3545f397741ca270dc78acf19f9a05e5
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-27 18:27:12 -0300

    feh

--- src/hooks/useTabsControl.tsx
+++ src/hooks/useTabsControl.tsx
@@ -1,49 +1,52 @@
-import { createContext, PropsWithChildren, useContext, useState } from "react"
+"use client";
 
-type tabType = 'configurationTabIsOpen' | 'userTabIsOpen' | 'navigationTabIsOpen'
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+type tabType =
+  | "configurationTabIsOpen"
+  | "userTabIsOpen"
+  | "navigationTabIsOpen";
 
 interface IShowTabOpen {
-  configurationTabIsOpen: boolean
-  userTabIsOpen: boolean
-  navigationTabIsOpen: boolean
+  configurationTabIsOpen: boolean;
+  userTabIsOpen: boolean;
+  navigationTabIsOpen: boolean;
 }
 
 interface ITabsControlProps {
-  showTabOpen: IShowTabOpen
-  handleToggleTabs: (tab: tabType) => void
+  showTabOpen: IShowTabOpen;
+  handleToggleTabs: (tab: tabType) => void;
 }
 
 interface ITabsControlContextProps extends PropsWithChildren {}
 
+const TabsControlContext = createContext({} as ITabsControlProps);
 
-const TabsControlContext = createContext({} as ITabsControlProps)
-
-
-export const TabsControlProvider = ({children}: ITabsControlContextProps) => {
-  
-
+export const TabsControlProvider = ({ children }: ITabsControlContextProps) => {
   const [showTabOpen, setShowTabOpen] = useState({
     configurationTabIsOpen: false,
     userTabIsOpen: false,
-    navigationTabIsOpen: false
-  })
+    navigationTabIsOpen: false,
+  });
 
   function handleToggleTabs(tab: tabType) {
-    setShowTabOpen(prev => {
-      const newObj: IShowTabOpen = {} as IShowTabOpen
-      Object.keys(prev).forEach(e => newObj[e as tabType] = e === tab && !prev[e])
-      return newObj
-    })
+    setShowTabOpen((prev) => {
+      const newObj: IShowTabOpen = {} as IShowTabOpen;
+      Object.keys(prev).forEach(
+        (e) => (newObj[e as tabType] = e === tab && !prev[e])
+      );
+      return newObj;
+    });
   }
 
   return (
-    <TabsControlContext.Provider value={{showTabOpen, handleToggleTabs}}>
+    <TabsControlContext.Provider value={{ showTabOpen, handleToggleTabs }}>
       {children}
     </TabsControlContext.Provider>
-  )
-}
+  );
+};
 
 export const useTabsControl = () => {
-  const context = useContext(TabsControlContext)
-  return context
-}
+  const context = useContext(TabsControlContext);
+  return context;
+};
