f159d36a53b0eb6b87698dfe922c809928c3de7c
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-28 18:45:57 -0300

    feat(Carousel): add rendering of cards dynamically

--- src/app/_components/CarouselSection.tsx
+++ src/app/_components/CarouselSection.tsx
@@ -1,15 +1,16 @@
 import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
+type TData = { id: number; name: string };
 interface ICarouselSectionProps {
   title: string;
-  data: string[];
+  data: TData[];
 }
 
-export const CarouselSection = () => (
+export const CarouselSection = ({ title, data }: ICarouselSectionProps) => (
   <div className="mt-[4rem]">
     <header className="w-full flex justify-between mb-8">
-      <h2 className="text-lg font-medium">Visitados Recentemente</h2>
+      <h2 className="text-lg font-medium">{title}</h2>
       <ArrowRight width={20} height={20} color="#DFEAEC" />
     </header>
     <Carousel
@@ -19,36 +20,13 @@ export const CarouselSection = () => (
       }}
     >
       <CarouselContent className="w-full -ml-6">
-        <CarouselItem className="basis-[48%] pl-6">
-          <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
-            {" "}
-            Matéria 1
-          </div>
-        </CarouselItem>
-        <CarouselItem className="basis-[48%] pl-6">
-          <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
-            {" "}
-            Matéria 1
-          </div>
-        </CarouselItem>
-        <CarouselItem className="basis-[48%] pl-6">
-          <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
-            {" "}
-            Matéria 1
-          </div>
-        </CarouselItem>
-        <CarouselItem className="basis-[48%] pl-6">
-          <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
-            {" "}
-            Matéria 1
-          </div>
-        </CarouselItem>
-        <CarouselItem className="basis-[48%] pl-6 -mr-6">
-          <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
-            {" "}
-            Matéria 1
-          </div>
-        </CarouselItem>
+        {data.map((e) => (
+          <CarouselItem className="basis-[48%] pl-6" key={e.id}>
+            <div className="flex justify-center items-center h-[calc(48vw*2/3)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">
+              {e.name}
+            </div>
+          </CarouselItem>
+        ))}
       </CarouselContent>
     </Carousel>
   </div>
