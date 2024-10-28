4f291962a741772461e36a0bbc80638073dab909
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-28 18:44:22 -0300

    feat(data): add fake data for site dynamics

---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const subjectsData = [
+  { id: 1, name: "Materia 1" }, 
+  { id: 1, name: "Materia 2" }, 
+  { id: 1, name: "Materia 3" }, 
+  { id: 1, name: "Materia 4" }, 
+  { id: 1, name: "Materia 5" } 
+]
+export const listData = [  
+  { id: 1, name: "Lista 1" }, 
+  { id: 1, name: "Lista 2" }, 
+  { id: 1, name: "Lista 3" }, 
+  { id: 1, name: "Lista 4" }, 
+  { id: 1, name: "Lista 5" }  
+]
