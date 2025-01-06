fc2ff55eda5340f54c87c3330b74ae2c63482f94
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2025-01-06 08:57:45 -0300

    test verification_3

--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -12,29 +12,29 @@ import { FilterButton } from "../_components/FilterButton";
 export default function Page() {
   return (
     <TabsControlProvider>
-        <div className="relative h-full bg-[rgba(0,0,0,0.75)] p-6 overflow-hidden">
-          <Header />
-          <main>
-            <h2 className="text-xl font-bold mb-12">Matérias disponiveis</h2>
-            <div className="flex gap-4">
-              <SearchBar/>
-              <FilterButton />
-            </div>
-            <section className="flex flex-wrap gap-6 mt-9 w-full">
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-              <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">fé</div>
-            </section>
-          </main>
-          <ConfigurationTab/>
-          <UserTab/>
-          <NavTab/>
-        </div>
+      <div className="relative h-full bg-[rgba(0,0,0,0.75)] p-6 overflow-hidden">
+        <Header />
+        <main>
+          <h2 className="text-xl font-bold mb-12">Matérias disponiveis</h2>
+          <div className="flex gap-4">
+            <SearchBar />
+            <FilterButton />
+          </div>
+          <section className="flex flex-wrap gap-6 mt-9 w-full">
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+            <div className="flex justify-center items-center w-[calc((100%-1.5rem)/2)] h-[calc((100vw-1.5rem)*3/8)] bg-gradient-to-br from-[#1A99B2] to-[#0B414C] rounded-xl">Matéria</div>
+          </section>
+        </main>
+        <ConfigurationTab />
+        <UserTab />
+        <NavTab />
+      </div>
     </TabsControlProvider>
   );
 }
